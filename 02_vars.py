10116138e84c4c293e468d4f86ffb3eb56c0dbaf
Jaime A Perez <82597268+Jaime-A-Perez@users.noreply.github.com>
2022-11-02 17:07:02 -0500

    Variables

---
+++ 02_vars.py
@@ -0,0 +1,20 @@
+print("Hola, vars")
+
+#This is a variable whit a string
+my_name = "Nicolas"
+print(my_name)
+
+#This is a variable whit a number
+my_age = 12
+print(my_age)
+
+my_name = "Santiago"
+print("aqui cambio", my_name)
+
+
+#Input
+my_name = input("What is your name? ")
+print("the new value of variable my_name is: ", my_name)
+
+my_age = input("How old are you? ")
+print("the new value of variable my_age is: ", my_age)
