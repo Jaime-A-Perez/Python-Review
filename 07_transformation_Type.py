3e95c488d37dd0d66b5bd7ca9e0f0702cd8f613e
Jaime A Perez <82597268+Jaime-A-Perez@users.noreply.github.com>
2022-11-03 17:49:16 -0500

    Arithmetic operators

--- 07_transformation_Type.py
+++ 07_transformation_Type.py
@@ -4,6 +4,6 @@ print("I am " + str(age) + " years old")
 print(f"I am {age} years old")
 
 age = input("How old are you?")
-age = int(age)
-age += 10
+age += int(age)
+#age += 10
 print(f"In 10 years, you will be {age} years old")
---
+++ 08_arith.py
@@ -0,0 +1,15 @@
+print(10 + 10)
+print(10 - 4)
+print(10 * 3)
+print(10 / 2)
+print(10 % 3)
+print(10 // 3)
+print(10 ** 2)
+print("""
+      Parentesis
+      Exponent
+      Multiplication
+      Division
+      Addition
+      Sustraction
+        """)
