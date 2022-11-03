4a0309d20817fe657d1fb2af4cde1605549eed59
Jaime A Perez <82597268+Jaime-A-Perez@users.noreply.github.com>
2022-11-03 15:17:15 -0500

    transformation of type

---
+++ 07_transformation_Type.py
@@ -0,0 +1,9 @@
+age = 12
+
+print("I am " + str(age) + " years old")
+print(f"I am {age} years old")
+
+age = input("How old are you?")
+age = int(age)
+age += 10
+print(f"In 10 years, you will be {age} years old")
