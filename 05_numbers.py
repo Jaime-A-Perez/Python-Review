e403cda77cd96105723ed874510a1048a7fd51bb
Jaime A Perez <82597268+Jaime-A-Perez@users.noreply.github.com>
2022-11-03 11:38:34 -0500

    Integers and floats

---
+++ 05_numbers.py
@@ -0,0 +1,32 @@
+
+int_type = 33
+print(type(int))
+
+float_type = 3.4
+print(type(float_type))
+
+int_type += 1
+print(type(int))
+
+float_type -= 0.3
+print(type(float_type))
+
+int_type *= float_type
+print(type(int))
+
+float_type /= int_type
+print(type(float_type))
+
+# Budget chalenge
+
+january_budget = 200
+febrary_budget = 402
+march_budget   = 394
+july_budget    = 604
+
+first_quarter_budget = january_budget + febrary_budget + march_budget
+
+print(f" The  budget of first quarter is: {first_quarter_budget}")
+
+
+
