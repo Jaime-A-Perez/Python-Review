df83a349c845585074cfef4375a706053dabc253
Jaime A Perez <82597268+Jaime-A-Perez@users.noreply.github.com>
2022-11-04 13:34:57 -0500

    Float comparations

---
+++ 10_float.py
@@ -0,0 +1,13 @@
+y = 1.1 + 2.2
+print(y)
+print("This is for the py make sum whit the binari number")
+
+ #Format whit string
+y_str = format(y, ".2g")
+
+print(f"y_str = {y_str}")
+
+# Method whit numbers
+tolerance = 0.01
+
+print(abs(y - 3.3 ) < tolerance)
