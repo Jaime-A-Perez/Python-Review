bcf15beb62034de17811b9f30bbefc0eb0c2d1a5
Jaime A Perez <82597268+Jaime-A-Perez@users.noreply.github.com>
2022-11-04 15:22:29 -0500

    Strings Methods

---
+++ 13.Conditionals_proyect.py
@@ -0,0 +1,40 @@
+user_choice = input("rock paper scissors : ")
+computer_choice = "rock"
+
+if user_choice == computer_choice:
+    print(""" 
+          Tie!""")
+elif user_choice == "rock":
+    if computer_choice == "paper":
+        print("""
+              paper beast stone
+              COMPUTER WINS!
+              """)
+    else: 
+        print("""
+              stone beast scissors
+              USER WINS!
+              """)
+elif user_choice == "paper":
+    if computer_choice == "scissors":
+        print("""
+              scissors beast paper
+              COMPUTER WINS!
+              """)
+    else: 
+        print("""
+              paper beast rock
+              USER WINS!
+              """)
+else:
+    if computer_choice == "rock":
+        print("""
+              rock best scissors
+              COMPUTER WINS!
+              """)
+    else: 
+        print("""
+              scissors beast paper
+              USER WINS!
+              """)
+                      
---
+++ 14.Strings_methods.py
@@ -0,0 +1,23 @@
+text = "She knows develop with Python"
+
+print("len(text)", len(text))
+
+print("uperCase:        ", text.upper())
+
+print("lowerCase:       ", text.lower())
+
+print("count of 'a' into text:  ", text.count("a"))
+
+print("swapCase:        ",text.swapcase())
+
+print("Verify if start with:    ", text.startswith('She'))
+
+print("Verify if end with:    ", text.endswith('Python'))
+
+print("Replace 'first' with 'second': ", text.replace("Python", "Go"))
+
+text2 = "hello, how are you?"
+
+print("Capitalize:      ", text2.capitalize())
+
+print("How a Title:     ", text2.title())
