e479a710b3a3fbb4ec0909354a6aa10aca1152ff
Jaime A Perez <82597268+Jaime-A-Perez@users.noreply.github.com>
2022-11-04 16:26:34 -0500

    Lists

---
+++ 16_list.py
@@ -0,0 +1,10 @@
+numbers = [2, 4, 5, 6]
+print(numbers)
+print(type(numbers))
+
+text = "si"
+# text[0] = "n"
+# The strings are inmutables and can't be modify ot this form
+
+numbers[2] = 3
+print(numbers)
