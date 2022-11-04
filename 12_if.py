d656e5c602b28e265c363cabd8dd119cc0367963
Jaime A Perez <82597268+Jaime-A-Perez@users.noreply.github.com>
2022-11-04 13:37:33 -0500

    if, elif, else

---
+++ 12_if.py
@@ -0,0 +1,13 @@
+# verify par or inpar
+
+number = input('Write the integer ')
+
+if int(number) :
+    print("The characters that you typed aren't integer ")
+else :
+    print("o")
+
+if number % 2 == 0 :
+    print('     The number is par')
+elif number % 2 != 0 :
+    print('     The number is inpar')
