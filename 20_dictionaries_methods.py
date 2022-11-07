8e950a026ea5c9cc6795476a78e79408af099e4f
Jaime A Perez <82597268+Jaime-A-Perez@users.noreply.github.com>
2022-11-07 16:10:38 -0500

    Dictionaries Methods

---
+++ 20_dictionaries_methods.py
@@ -0,0 +1,28 @@
+person = {
+    'name'      : 'Nico',
+    'last_name' : 'Molina',
+    'langs'     : ['python, javascript'],
+    'age'       : 59
+}
+
+print(f"person {person}")
+
+
+person['name'] = 'Santi'
+person['age'] += 2
+person['langs'].append('rust')
+
+print(f"person modified{person}")
+
+
+del person['last_name']
+person.pop('age')
+
+print(f"person with deleted methods {person}")
+
+
+print(f"items()   = {person.items()}")
+
+print(f"keys()    = {person.keys()}")
+
+print(f"values()  = {person.values()}")
