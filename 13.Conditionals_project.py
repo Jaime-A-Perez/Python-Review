491b5e31a20c54c8b12657547032b28f0177824a
Jaime A Perez <82597268+Jaime-A-Perez@users.noreply.github.com>
2022-11-10 11:24:14 -0500

    Finish Project ROCK PAPER SCISSORS

--- 13.Conditionals_project.py
+++ 13.Conditionals_project.py
@@ -1,53 +1,88 @@
 import random
 options = ("rock", "paper", "scissors")
-user_choice = input("rock paper scissors : ")
-if not user_choice in options:
-    print("""
-          ** Invalid Option **
-          
-          * try again *
-          
-          """)
+is_there_a_winer = False
+rounds = 0
+computter_wins = 0
+user_wins = 0
+while not is_there_a_winer:
+    print()
+    print('*' * 30)
+    print('         ROUND', rounds)
+    print(f"computer = {computter_wins}")
+    print(f"user = {user_wins}")
+    print('*' * 10)
     user_choice = input("rock paper scissors : ")
+    if not user_choice in options:
+        print("""
+              ** Invalid Option **
 
-computer_choice = random.choice(options)
+              * try again *
+
+              """)
+        user_choice = input("rock paper scissors : ")
+
+    computer_choice = random.choice(options)
 
 
 
-if user_choice == computer_choice:
-    print(""" 
-          Tie!""")
-elif user_choice == "rock":
-    if computer_choice == "paper":
+    if user_choice == computer_choice:
+        print(""" 
+              Tie!""")
+    elif user_choice == "rock":
+        if computer_choice == "paper":
+            print("""
+                  paper beast stone
+                  COMPUTER WINS!
+                  """)
+            computter_wins += 1
+        else: 
+            print("""
+                  stone beast scissors
+                  USER WINS!
+                  """)
+            user_wins += 1
+    elif user_choice == "paper":
+        if computer_choice == "scissors":
+            print("""
+                  scissors beast paper
+                  COMPUTER WINS!
+                  """)
+            computter_wins += 1
+        else: 
+            print("""
+                  paper beast rock
+                  USER WINS!
+                  """)
+            user_wins += 1
+    else:
+        if computer_choice == "rock":
+            print("""
+                  rock best scissors
+                  COMPUTER WINS!
+                  """)
+            computter_wins += 1
+        else: 
+            print("""
+                  scissors beast paper
+                  USER WINS!
+                  """)
+            user_wins += 1
+
+    if computter_wins == 2:
         print("""
-              paper beast stone
-              COMPUTER WINS!
-              """)
-    else: 
+              
+              THE FINAL WINNER IS THE COMPUTTER""")
+        print()
+        print('*' * 30)
+        break
+    if user_wins == 2:
         print("""
-              stone beast scissors
-              USER WINS!
-              """)
-elif user_choice == "paper":
-    if computer_choice == "scissors":
-        print("""
-              scissors beast paper
-              COMPUTER WINS!
-              """)
-    else: 
-        print("""
-              paper beast rock
-              USER WINS!
-              """)
-else:
-    if computer_choice == "rock":
-        print("""
-              rock best scissors
-              COMPUTER WINS!
-              """)
-    else: 
-        print("""
-              scissors beast paper
-              USER WINS!
-              """)
-                      
+              
+              THE FINAL WINNER IS THE USER""")
+        print()
+        print('*' * 30)
+        break
+
+
+    rounds += 1
+    
